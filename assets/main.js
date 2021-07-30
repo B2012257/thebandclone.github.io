74453e955d1732fc1659e6ea0ae482189b9bc34e
ThaiStar <thaib2012257@student.ctu.edu.vn>
2021-07-30 22:48:52 +0700

    UP

---
+++ assets/main.js
@@ -0,0 +1,36 @@
+// var boolen = true;
+// var name ="ThaiPham";
+// console.log(boolen);
+// Phep so sanh tra ve kieu boolean
+/**
+ * null
+ * 0
+ * false
+ * NaN
+ * undefined
+ * "" - ''
+ */
+
+// var isSuccess = 0;
+// if(isSuccess) {
+//     console.log("Dieu kien dung");
+// } else {
+//     console.log("Dieu kien sai");
+// }
+
+/*var array = [
+    'PHP', 'Javascript', 'C++',
+    'Java'
+]
+console.log(array);
+c
+confirm("Như có khùng ko? nếu có nhấn OK!") */
+/**
+ * === so sanh ca data type va value
+ * !== so sanh ca data type va value
+ */
+// var name = 10;
+// var result;
+// result = name == 0? true : false;
+// console.log(result);
+// console.log(!!name);
